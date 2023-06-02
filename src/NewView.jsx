29acc482892167cb6fbac6a20d6d67d19fac2fb5
Abdul Hadi <hadi2408466@gmail.com>
2023-06-03 00:40:31 +0500

    last change to style

--- src/NewView.jsx
+++ src/NewView.jsx
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
   description: {
     color: 'white',
     textTransform: 'capitalize',
-    fontSize: 25,
+    fontSize: 22,
     right: 15,
   },
   container: {
