597564ec6396170eb28423b803fa291151a03fc5
Abdul Hadi <hadi2408466@gmail.com>
2023-06-08 23:52:00 +0500

    ads added

--- App.tsx
+++ App.tsx
@@ -1,23 +1,28 @@
 import 'react-native-gesture-handler';
-import RNBootSplash from "react-native-bootsplash";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import Navigation from "./src/Navigation";
+import RNBootSplash from 'react-native-bootsplash';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/Navigation';
 // import Redux
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import reducer from "./src/redux/reducers/reducer";
-import logger from "redux-logger";
-
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './src/redux/reducers/reducer';
+import logger from 'redux-logger';
+import {
+  RewardedAd,
+  RewardedAdEventType,
+  TestIds,
+} from 'react-native-google-mobile-ads';
+import mobileAds from 'react-native-google-mobile-ads';
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default function App(): JSX.Element {
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
     </Provider>
   );
 }
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Weathercall from './Weathercall';
 import Details from './Details';
@@ -9,159 +9,222 @@ import {Image, View, Text, StyleSheet} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {BlurView} from '@react-native-community/blur';
 import LinearGradient from 'react-native-linear-gradient';
+import {
+  RewardedAd,
+  RewardedAdEventType,
+  TestIds,
+  BannerAd,
+  BannerAdSize,
+} from 'react-native-google-mobile-ads';
 
+const adUnitId = __DEV__
+  ? TestIds.REWARDED
+  : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
+const rewarded = RewardedAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+});
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const unsubscribeLoaded = rewarded.addAdEventListener(
+      RewardedAdEventType.LOADED,
+      () => {
+        setLoaded(true);
+      },
+    );
+    const unsubscribeEarned = rewarded.addAdEventListener(
+      RewardedAdEventType.EARNED_REWARD,
+      reward => {
+        console.log('User earned reward of ', reward);
+      },
+      // setLoaded(true),
+    );
+
+    // Start loading the rewarded ad straight away
+    rewarded.load();
+
+    // Unsubscribe from events on unmount
+    return () => {
+      unsubscribeLoaded();
+      unsubscribeEarned();
+    };
+  }, []);
+
+  // No advert ready to show yet
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarShowLabel: false,
-        tabBarStyle: [
-          {
-            borderTopWidth: 0,
-            position: 'absolute',
-            height: 70,
-            bottom: 10,
-            borderRadius: 200,
-          },
-          null,
-        ],
-        tabBarBackground: () => (
-          <View>
-            <BlurView
-              style={styles.absolute}
-              blurType="dark"
-              blurAmount={10}
-              reducedTransparencyFallbackColor="black">
-              <LinearGradient
-                start={{x: 0, y: 0}}
-                end={{x: 1, y: 1}}
-                colors={['rgba(46,51,90,0.50)', 'rgba(51,48,102,0.20)']}
-                style={{
-                  borderWidth: 1,
-                  borderColor: 'grey',
-                  height: 80,
-                  borderRadius: 20,
-                }}
-              />
-            </BlurView>
-          </View>
-        ),
-      })}>
-      <Tab.Screen
-        name="Home"
-        component={Weathercall}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({focused, tintColor}) => (
-            <TouchableOpacity
-              style={{
-                top: 5,
-                paddingLeft: 10,
-                paddingRight: 10,
-                backgroundColor: focused ? '#fff' : null,
-                alignItems: 'center',
-                borderWidth: focused ? 5 : 0,
-                borderColor: focused ? '#fff' : null,
-                borderRadius: focused ? 100 : 0,
-              }}>
-              <Image
-                source={require('../assets/icons/home.png')}
-                resizeMode="contain"
-                style={{
-                  width: focused ? 25 : 20,
-                  height: 40,
-                  tintColor: focused ? '#000' : '#fff',
-                }}
-              />
-              <Text
-                style={{
-                  color: focused ? '#000' : '#fff',
-                  fontSize: focused ? 12 : 10,
-                  fontWeight: focused ? '600': '400',
-                }}>
-                HOME
-              </Text>
-            </TouchableOpacity>
+    <View
+      style={{
+        flex: 1,
+      }}>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarShowLabel: false,
+          tabBarStyle: [
+            {
+              borderTopWidth: 0,
+              position: 'absolute',
+              height: 70,
+              bottom: 10,
+              borderRadius: 200,
+            },
+            null,
+          ],
+          tabBarBackground: () => (
+            <View>
+              <BlurView
+                style={styles.absolute}
+                blurType="dark"
+                blurAmount={10}
+                reducedTransparencyFallbackColor="black">
+                <LinearGradient
+                  start={{x: 0, y: 0}}
+                  end={{x: 1, y: 1}}
+                  colors={['rgba(46,51,90,0.50)', 'rgba(51,48,102,0.20)']}
+                  style={{
+                    borderWidth: 1,
+                    borderColor: 'grey',
+                    height: 80,
+                    borderRadius: 20,
+                  }}
+                />
+              </BlurView>
+            </View>
           ),
+        })}>
+        <Tab.Screen
+          name="Home"
+          component={Weathercall}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused, tintColor}) => (
+              <TouchableOpacity
+                style={{
+                  top: 5,
+                  paddingLeft: 10,
+                  paddingRight: 10,
+                  backgroundColor: focused ? '#fff' : null,
+                  alignItems: 'center',
+                  borderWidth: focused ? 5 : 0,
+                  borderColor: focused ? '#fff' : null,
+                  borderRadius: focused ? 100 : 0,
+                }}>
+                <Image
+                  source={require('../assets/icons/home.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: focused ? 25 : 20,
+                    height: 40,
+                    tintColor: focused ? '#000' : '#fff',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: focused ? '#000' : '#fff',
+                    fontSize: focused ? 12 : 10,
+                    fontWeight: focused ? '600' : '400',
+                  }}>
+                  HOME
+                </Text>
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused, tintColor}) => (
+              <TouchableOpacity
+                style={{
+                  top: 5,
+                  paddingLeft: 10,
+                  paddingRight: 10,
+                  backgroundColor: focused ? '#fff' : null,
+                  alignItems: 'center',
+                  borderWidth: focused ? 5 : 0,
+                  borderColor: focused ? '#fff' : null,
+                  borderRadius: focused ? 100 : 0,
+                }}>
+                <Image
+                  source={require('../assets/icons/menu.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: focused ? 25 : 20,
+                    height: 40,
+                    tintColor: focused ? '#000' : '#fff',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: focused ? '#000' : '#fff',
+                    fontSize: focused ? 12 : 10,
+                    fontWeight: focused ? '600' : '400',
+                  }}>
+                  DETAILS
+                </Text>
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Five Day"
+          component={FivedayForecast}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({focused, tintColor}) => (
+              <TouchableOpacity
+                style={{
+                  top: 5,
+                  paddingLeft: 10,
+                  paddingRight: 10,
+                  backgroundColor: focused ? '#fff' : null,
+                  alignItems: 'center',
+                  borderWidth: focused ? 5 : 0,
+                  borderColor: focused ? '#fff' : null,
+                  borderRadius: focused ? 100 : 0,
+                }}
+                onPress={() => {
+                  rewarded.show();
+                }}>
+                <Image
+                  source={require('../assets/icons/clouds.png')}
+                  resizeMode="contain"
+                  style={{
+                    width: focused ? 25 : 20,
+                    height: 40,
+                    tintColor: focused ? '#000' : '#fff',
+                  }}
+                />
+                <Text
+                  style={{
+                    color: focused ? '#000' : '#fff',
+                    fontSize: focused ? 12 : 10,
+                    fontWeight: focused ? '600' : '400',
+                  }}>
+                  FIVE DAY
+                </Text>
+              </TouchableOpacity>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+      <BannerAd
+        unitId={adUnitId}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
         }}
       />
-      <Tab.Screen
-        name="Details"
-        component={Details}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({focused, tintColor}) => (
-            <TouchableOpacity style={{
-              top: 5,
-              paddingLeft: 10,
-              paddingRight: 10,
-              backgroundColor: focused ? '#fff' : null,
-              alignItems: 'center',
-              borderWidth: focused ? 5 : 0,
-              borderColor: focused ? '#fff' : null,
-              borderRadius: focused ? 100 : 0,
-            }}>
-              <Image
-                source={require('../assets/icons/menu.png')}
-                resizeMode="contain"
-                style={{
-                  width: focused ? 25 : 20,
-                  height: 40,
-                  tintColor: focused ? '#000' : '#fff',
-                }}
-              />
-              <Text
-                style={{
-                  color: focused ? '#000' : '#fff',
-                  fontSize: focused ? 12 : 10,
-                  fontWeight: focused ? '600': '400',
-                }}>
-                DETAILS
-              </Text>
-            </TouchableOpacity>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Five Day"
-        component={FivedayForecast}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({focused, tintColor}) => (
-            <TouchableOpacity style={{
-              top: 5,
-              paddingLeft: 10,
-              paddingRight: 10,
-              backgroundColor: focused ? '#fff' : null,
-              alignItems: 'center',
-              borderWidth: focused ? 5 : 0,
-              borderColor: focused ? '#fff' : null,
-              borderRadius: focused ? 100 : 0,
-            }}>
-              <Image
-                source={require('../assets/icons/clouds.png')}
-                resizeMode="contain"
-                style={{
-                  width: focused ? 25 : 20,
-                  height: 40,
-                  tintColor: focused ? '#000' : '#fff',
-                }}
-              />
-              <Text
-                style={{
-                  color: focused ? '#000' : '#fff',
-                  fontSize: focused ? 12 : 10,
-                  fontWeight: focused ? '600': '400',
-                }}>
-                FIVE DAY
-              </Text>
-            </TouchableOpacity>
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    </View>
   );
 }
 const Stack = createStackNavigator();
