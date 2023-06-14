0571d4cd7ee35ca9982c087f56abf41c759ee08e
Abdul Hadi <hadi2408466@gmail.com>
2023-06-14 21:07:38 +0500

    ads removed temp

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -9,26 +9,22 @@ import {Image, View, Text, StyleSheet} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {BlurView} from '@react-native-community/blur';
 import LinearGradient from 'react-native-linear-gradient';
-import {
-  InterstitialAd,
-  AdEventType,
-  TestIds,
-  BannerAd,
-  BannerAdSize,
-} from 'react-native-google-mobile-ads';
+// import {
+//   InterstitialAd,
+//   AdEventType,
+//   TestIds,
+//   BannerAd,
+//   BannerAdSize,
+// } from 'react-native-google-mobile-ads';
 
-const adUnitId = __DEV__
-  ? TestIds.INTERSTITIAL
-  : 'ca-app-pub-9315049673582381/4958846198';
+// const adUnitId = 'ca-app-pub-9315049673582381/4958846198';
 
-const BannerAdUnitID = __DEV__
-  ? TestIds.BANNER
-  : 'ca-app-pub-9315049673582381/2715826235';
+// const BannerAdUnitID = 'ca-app-pub-9315049673582381/2715826235';
 
-const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
-  requestNonPersonalizedAdsOnly: true,
-  keywords: ['fashion', 'clothing'],
-});
+// const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
+//   requestNonPersonalizedAdsOnly: true,
+//   keywords: ['fashion', 'clothing'],
+// });
 
 const Tab = createBottomTabNavigator();
 
@@ -166,9 +162,10 @@ function Tabs(props) {
                   borderColor: focused ? '#fff' : null,
                   borderRadius: focused ? 100 : 0,
                 }}
-                onPress={() => {
-                  props.func();
-                }}>
+                // onPress={() => {
+                //   props.func();
+                // }}
+              >
                 <Image
                   source={require('../assets/icons/clouds.png')}
                   resizeMode="contain"
@@ -191,54 +188,54 @@ function Tabs(props) {
           }}
         />
       </Tab.Navigator>
-      <BannerAd
-        unitId={BannerAdUnitID}
-        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
-        requestOptions={{
-          requestNonPersonalizedAdsOnly: true,
-        }}
-      />
+      {/* <BannerAd
+      // unitId={BannerAdUnitID}
+      // size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      // requestOptions={{
+      //   requestNonPersonalizedAdsOnly: true,
+      // }}
+      /> */}
     </View>
   );
 }
 const Stack = createStackNavigator();
 export default function Navigation() {
-  const [interstitialLoaded, setInterstitialLoaded] = useState(false);
-  const loadInterstitial = () => {
-    const unsubscribeLoaded = interstitial.addAdEventListener(
-      AdEventType.LOADED,
-      () => {
-        setInterstitialLoaded(true);
-      },
-    );
+  // const [interstitialLoaded, setInterstitialLoaded] = useState(false);
+  // const loadInterstitial = () => {
+  //   const unsubscribeLoaded = interstitial.addAdEventListener(
+  //     AdEventType.LOADED,
+  //     () => {
+  //       setInterstitialLoaded(true);
+  //     },
+  //   );
 
-    const unsubscribeClosed = interstitial.addAdEventListener(
-      AdEventType.CLOSED,
-      () => {
-        setInterstitialLoaded(false);
-        interstitial.load();
-      },
-    );
+  //   const unsubscribeClosed = interstitial.addAdEventListener(
+  //     AdEventType.CLOSED,
+  //     () => {
+  //       setInterstitialLoaded(false);
+  //       interstitial.load();
+  //     },
+  //   );
 
-    interstitial.load();
+  //   interstitial.load();
 
-    return () => {
-      unsubscribeClosed();
-      unsubscribeLoaded();
-    };
-  };
-  useEffect(() => {
-    const unsubscribeInterstitialEvents = loadInterstitial();
+  //   return () => {
+  //     unsubscribeClosed();
+  //     unsubscribeLoaded();
+  //   };
+  // };
+  // useEffect(() => {
+  //   const unsubscribeInterstitialEvents = loadInterstitial();
 
-    return () => {
-      unsubscribeInterstitialEvents();
-    };
-  }, []);
-  const interstitialAdsShow = () => {
-    if (interstitialLoaded) {
-      interstitial.show();
-    }
-  };
+  //   return () => {
+  //     unsubscribeInterstitialEvents();
+  //   };
+  // }, []);
+  // const interstitialAdsShow = () => {
+  //   if (interstitialLoaded) {
+  //     interstitial.show();
+  //   }
+  // };
   return (
     <Stack.Navigator initialRouteName="splash">
       <Stack.Screen
@@ -250,7 +247,11 @@ export default function Navigation() {
         name="Tab"
         // component={Tabs}
         options={{headerShown: false}}>
-        {() => <Tabs func={interstitialAdsShow} />}
+        {() => (
+          <Tabs
+          // func={interstitialAdsShow}
+          />
+        )}
       </Stack.Screen>
     </Stack.Navigator>
   );
